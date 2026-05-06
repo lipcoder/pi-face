7b798bb5ddc40a8b6e0d5c88d39cade6b89653c6
lipcoder <lipcoder@foxmail.com>
2026-05-06 21:47:52 +0800

    摄像头加上接口设计

---
+++ internal/recognition/recognition.go
@@ -0,0 +1,8 @@
+package recognition
+
+type Recognition interface {
+	PostImage(imgBytes []byte) ([]byte, error)
+	GetFaceCount(respBody []byte) (int, error)
+	GetFaceEmbedding(respBody []byte, rank int) ([]float64, error)
+	EmbeddingToPGVector(embedding []float64) string
+}
